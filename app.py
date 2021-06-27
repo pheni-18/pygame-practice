1b26063946cdf7fd28f01d3b9e9a16f89a6f27fa
pheni-18 <pheni.engineer@gmail.com>
2021-06-27 19:42:43 +0900

    Add player

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ IMAGE_DIR = os.path.join(APP_DIR, 'images')
 
 screen = pg.display.set_mode((640, 480))
 
-icon_path = os.path.join(IMAGE_DIR, 'icon.png')
-icon_image = pg.image.load(icon_path)
+icon_image_path = os.path.join(IMAGE_DIR, 'icon.png')
+icon_image = pg.image.load(icon_image_path)
 pg.display.set_icon(icon_image)
 pg.display.set_caption('Pygame Practice')
 # pg.mouse.set_visible(False)
@@ -21,11 +21,21 @@ background_image = pg.image.load(background_image_path)
 screen.blit(background_image, (0, 0))
 pg.display.flip()
 
-running = True
-while running:
-    for even in pg.event.get():
-        if even.type == pg.QUIT:
-            running = False
+player_image_path = os.path.join(IMAGE_DIR, 'icon.png')
+player_image = pg.image.load(player_image_path)
+
+
+stop = False
+while not stop:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            stop = False
+        elif event.type == pg.KEYDOWN:
+            if event.key == pg.K_ESCAPE:
+                stop = True
+
+    screen.blit(player_image, (300, 300))
+    pg.display.update()
 
 
 pg.quit()

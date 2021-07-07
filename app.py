e345c09f7ec098758cea644e0f3d637b101d20fc
pheni-18 <pheni.engineer@gmail.com>
2021-07-07 21:29:14 +0900

    Player can move

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ APP_DIR = os.path.split(os.path.abspath(__file__))[0]
 IMAGE_DIR = os.path.join(APP_DIR, 'images')
 
 
-screen = pg.display.set_mode((640, 480))
+screen = pg.display.set_mode((640, 426))
 
 icon_image_path = os.path.join(IMAGE_DIR, 'icon.png')
 icon_image = pg.image.load(icon_image_path)
@@ -18,12 +18,13 @@ pg.display.set_caption('Pygame Practice')
 
 background_image_path = os.path.join(IMAGE_DIR,'background.jpeg')
 background_image = pg.image.load(background_image_path)
-screen.blit(background_image, (0, 0))
-pg.display.flip()
 
 player_image_path = os.path.join(IMAGE_DIR, 'icon.png')
 player_image = pg.image.load(player_image_path)
+player_x = 300
+player_y = 300
 
+MOVE_DISTANCE = 5
 
 stop = False
 while not stop:
@@ -33,8 +34,18 @@ while not stop:
         elif event.type == pg.KEYDOWN:
             if event.key == pg.K_ESCAPE:
                 stop = True
+            if event.key == pg.K_UP:
+                player_y -= MOVE_DISTANCE
+            if event.key == pg.K_DOWN:
+                player_y += MOVE_DISTANCE
+            if event.key == pg.K_LEFT:
+                player_x -= MOVE_DISTANCE
+            if event.key == pg.K_RIGHT:
+                player_x += MOVE_DISTANCE
 
-    screen.blit(player_image, (300, 300))
+    screen.blit(background_image, (0, 0))
+    screen.blit(player_image, (player_x, player_y))
+
     pg.display.update()
 
 

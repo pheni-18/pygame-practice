a62aa129a13534e15189eedc1ebaf60f4a142564
pheni-18 <pheni.engineer@gmail.com>
2021-07-08 21:38:15 +0900

    Add obstacles

--- app.py
+++ app.py
@@ -8,7 +8,9 @@ APP_DIR = os.path.split(os.path.abspath(__file__))[0]
 IMAGE_DIR = os.path.join(APP_DIR, 'images')
 
 
-screen = pg.display.set_mode((640, 426))
+SCREEN_WIDTH = 640
+SCREEN_HEIGHT = 426
+screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 icon_image_path = os.path.join(IMAGE_DIR, 'icon.png')
 icon_image = pg.image.load(icon_image_path)
@@ -23,8 +25,40 @@ player_image_path = os.path.join(IMAGE_DIR, 'icon.png')
 player_image = pg.image.load(player_image_path)
 player_x = 300
 player_y = 300
+PLAYER_WIDTH = 24
+PLAYER_HEIGHT = 32
 
 MOVE_DISTANCE = 1
+
+obstacle_image_path = os.path.join(IMAGE_DIR, 'obstacle.png')
+obstacle_image = pg.image.load(obstacle_image_path)
+obstacle_rects = [(100, 100), (60, 250), (160, 320), (250, 120), (500, 220), (430, 50)]
+OBSTACLE_WIDTH = 129
+OBSTACLE_HEIGHT = 107
+
+used_screen = [[0] * (SCREEN_HEIGHT + 1) for _ in range(SCREEN_WIDTH + 1)]
+for obstacle_rect in obstacle_rects:
+    obstacle_x = obstacle_rect[0]
+    obstacle_y = obstacle_rect[1]
+    for x in range(obstacle_x, obstacle_x + OBSTACLE_WIDTH + 1):
+        for y in range(obstacle_y, obstacle_y + OBSTACLE_HEIGHT + 1):
+            if not ((0 <= x <= SCREEN_WIDTH) and (0 <= y <= SCREEN_HEIGHT)):
+                continue
+
+            used_screen[x][y] = 1
+
+
+def can_move(player_x: int, player_y: int):
+    for x in range(player_x, player_x + PLAYER_WIDTH + 1):
+        for y in range(player_y, player_y + PLAYER_HEIGHT + 1):
+            if not ((0 <= x <= SCREEN_WIDTH) and (0 <= y <= SCREEN_HEIGHT)):
+                continue
+
+            if used_screen[x][y] == 1:
+                return False
+
+    return True
+
 
 stop = False
 while not stop:
@@ -35,18 +69,25 @@ while not stop:
             if event.key == pg.K_ESCAPE:
                 stop = True
 
-    keystate = pg.key.get_pressed()
-    if keystate[pg.K_UP]:
-        player_y -= MOVE_DISTANCE
-    if keystate[pg.K_DOWN]:
-        player_y += MOVE_DISTANCE
-    if keystate[pg.K_LEFT]:
-        player_x -= MOVE_DISTANCE
-    if keystate[pg.K_RIGHT]:
-        player_x += MOVE_DISTANCE
+    key_state = pg.key.get_pressed()
+    tmp_player_rect = [player_x, player_y]
+    if key_state[pg.K_UP]:
+        tmp_player_rect[1] = player_y - MOVE_DISTANCE
+    if key_state[pg.K_DOWN]:
+        tmp_player_rect[1] = player_y + MOVE_DISTANCE
+    if key_state[pg.K_LEFT]:
+        tmp_player_rect[0] = player_x - MOVE_DISTANCE
+    if key_state[pg.K_RIGHT]:
+        tmp_player_rect[0] = player_x + MOVE_DISTANCE
+
+    if can_move(tmp_player_rect[0], tmp_player_rect[1]):
+        player_x, player_y = tmp_player_rect
 
     screen.blit(background_image, (0, 0))
     screen.blit(player_image, (player_x, player_y))
+
+    for obstacle_rect in obstacle_rects:
+        screen.blit(obstacle_image, obstacle_rect)
 
     pg.display.update()
 
